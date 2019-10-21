62ff24744250363bd3433fb2978a7ef5855b463b
Kyle Barnhouse <skyline6965@gmail.com>
2019-10-21 12:02:27 -0500

    updated example.js

--- models/example.js
+++ models/example.js
@@ -1,7 +1,28 @@
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
   var Example = sequelize.define("Example", {
     text: DataTypes.STRING,
     description: DataTypes.TEXT
   });
   return Example;
+};
+
+
+module.exports = function (sequelize, DataTypes) {
+  var User = sequelize.define("User", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    age: {
+      type: DataTypes.I
+    },
+    bio: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    picture: {
+      type: DataTypes.STRING
+    }
+  });
+  return User;
 };
--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,25 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+-- DROP DATABASE IF EXISTS exampledb;
+-- CREATE DATABASE exampledb;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+-- DROP DATABASE IF EXISTS testdb;
+-- CREATE DATABASE testdb;
+
+
+DROP DATABASE IF EXISTS main_content;
+CREATE DATABASE main_content;
+
+CREATE TABLE threads (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(50) NOT NULL,
+    body TEXT NOT NULL,
+    author VARCHAR(20),
+    PRIMARY KEY (id)
+)
+
+CREATE TABLE comments (
+    id INT NOT NULL AUTO_INCREMENT,
+    comments TEXT,
+    PRIMARY KEY (id)
+)
+
+
