a5e4f9d92b145f38528a594dedde3763c91d35bd
Daniel Masters <daniel.p.masters@gmail.com>
2019-10-22 13:49:00 -0500

    fixed html routes and began api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,28 +1,56 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Get all mustaches
-  app.get("/api/mustaches", function(req, res) {
-    db.user_db.findAll({
-      include: [db.Post]
-    }).then(function(user_db) {
-      res.json(user_db);
-    });
+  app.get("/api/users", function (req, res) {
+    db.user_db.findAll({})
+      .then(function (user_db) {
+        res.json(user_db);
+      });
   });
 
+  app.get("/api/users/:id", function (req, res) {
+    db.user.findOne({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(function (user_db) {
+        res.json(user_db)
+      })
+  })
+
   // Create a new example
-  app.post("/api/mustaches", function(req, res) {
-    db.user_db.create(req.body).then(function(user_db) {
+  app.post("/api/users", function (req, res) {
+    db.user_db.create({
+      name: req.body.name,
+      age: req.body.age,
+      bio: req.body.bio,
+      pictureURL: req.body.pictureURL
+    }).then(function (user_db) {
       res.json(user_db);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/mustaches/:id", function(req, res) {
-    db.user_db.destroy({ where: { id: req.params.id } }).then(function(
-      user_db
-    ) {
+  app.delete("/api/users/:id", function (req, res) {
+    db.user_db.destroy({ where: 
+      { 
+        id: req.params.id 
+      } 
+    }).then(function (user_db) {
       res.json(user_db);
     });
+  });
+
+  app.put("/api/users", function(req,res){
+    db.user_db.update(req.body,
+      {
+        where: {
+          id:req.body.id
+        }
+      }).then(function(user_db){
+        res.json(user_db)
+      });
   });
 };
