a958674dabb5e8ee7e40ca53ce9075559ae9b7cb
Kyle Barnhouse <skyline6965@gmail.com>
2019-10-23 19:08:44 -0500

    stashing changes to push/pull

--- routes/profileAPIRoutes.js
+++ routes/profileAPIRoutes.js
@@ -1,61 +1,58 @@
 /* eslint-disable camelcase */
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Get all profile
-  app.get("/api/profile", function(req, res) {
-    db.user_db.findAll({}).then(function(user_db) {
+  app.get("/api/profile", function (req, res) {
+    db.Profile.findAll({}).then(function (user_db) {
       res.json(user_db);
     });
   });
   //see one user
-  app.get("/api/profile/:id", function(req, res) {
-    db.user_db
-      .findOne({
+  app.get("/api/profile/:id", function (req, res) {
+    db.Profile.findOne({
         where: {
           id: req.params.id
         }
       })
-      .then(function(user_db) {
+      .then(function (user_db) {
         res.json(user_db);
       });
   });
 
-  // Create a new user
-  app.post("/api/profile", function(req, res) {
-    db.user_db
-      .create({
-        name: req.body.name,
-        age: req.body.age,
-        bio: req.body.bio,
-        pictureURL: req.body.pictureURL
-      })
-      .then(function(user_db) {
+  // Create a new profile
+  app.post("/api/profile", function (req, res) {  
+    console.log(req.body);
+    db.Profile.create({
+      name: req.body.name,
+      age: req.body.age,
+      bio: req.body.bio,
+      pictureURL: req.body.pictureURL
+    })
+      .then(function (user_db) {
         res.json(user_db);
       });
   });
 
   // Delete a user by id
-  app.delete("/api/profile/:id", function(req, res) {
-    db.user_db
-      .destroy({
+  app.delete("/api/profile/:id", function (req, res) {
+    db.Profile.destroy({
         where: {
           id: req.params.id
         }
       })
-      .then(function(user_db) {
+      .then(function (user_db) {
         res.json(user_db);
       });
   });
   //update user info
-  app.put("/api/profile", function(req, res) {
-    db.user_db
-      .update(req.body, {
+  app.put("/api/profile", function (req, res) {
+    db.Profile.update(req.body, {
         where: {
           id: req.body.id
         }
       })
-      .then(function(user_db) {
+      .then(function (user_db) {
         res.json(user_db);
       });
   });
